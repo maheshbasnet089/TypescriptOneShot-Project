ebe2eb3c1c5893468e3ca609f4a3950846726629
maheshbasnet089 <basnetmanish089@gmail.com>
2024-09-29 23:37:50 +0545

    made globalErrorHandler

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,8 @@ config()
 const envConfig = {
     port : process.env.PORT, 
     mongodbString : process.env.MONGODB_URI, 
-    backendUrl : process.env.BACKEND_URL
+    backendUrl : process.env.BACKEND_URL, 
+    environment : process.env.NODE_ENV
 }
 
 export default envConfig
--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,7 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import envConfig from "../config/config";
 
 
 
-const globalErrorHandler = (err,req:Request,res:Response,next:NextFunction)=>{
-
+const globalErrorHandler = (err:HttpError,req:Request,res:Response,next:NextFunction)=>{
+    const statusCode = err.statusCode || 500 
+    res.status(statusCode).json({
+        message : err.message, 
+        errorStack : envConfig.environment === 'development' ? err.stack : "Something went wrong"
+    })
 }
+
+export default globalErrorHandler
