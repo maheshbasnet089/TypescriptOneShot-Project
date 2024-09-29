24cb0ccb81d3930778aa00e7ab9f93b269005dc7
maheshbasnet089 <basnetmanish089@gmail.com>
2024-09-30 00:09:10 +0545

    backend complete

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,23 @@
 import express from 'express'
 import globalErrorHandler from './middlewares/globalErrorHandler'
 import noteRoute from './note/noteRoute'
+import cors from 'cors'
+import envConfig from './config/config'
 
 const app = express()
+// PARSE INCOMING JSON TO HANDLE UNDEFINED ERROR 
 app.use(express.json())
 
+// CORS CONFIGURATOIN 
+app.use(cors({
+    origin : envConfig.frontendUrl
+}))
+
 app.use("/api/notes",noteRoute)
+
+// IMAGE PUBLIC
 app.use(express.static("./src/uploads/"))
+
+// ERROR HANDLER
 app.use(globalErrorHandler)
 export default app
--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,8 @@ const envConfig = {
     port : process.env.PORT, 
     mongodbString : process.env.MONGODB_URI, 
     backendUrl : process.env.BACKEND_URL, 
-    environment : process.env.NODE_ENV
+    environment : process.env.NODE_ENV, 
+    frontendUrl : process.env.FRONTEND_URL
 }
 
 export default envConfig
